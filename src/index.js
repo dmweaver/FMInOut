6ddd6a01cfbe9240dbcbeecb95aad7c1b5d4f8cd
weaverd <david.weaver@dpird.wa.gov.au>
2022-10-13 08:55:58 +0800

    brought in config from FM

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
 import c3 from "c3";
 
-const options = {
-  bindto: "#chart",
-  axis: 
-    {x: {type: "category"}},
-  data: {
-    colors: {
-      Apples: "#ff0000",
-      Peaches: "#00ff00",
-      Bananas: "#0000ff",
+window.loadChart=function(json){
+  console.log(json);
+  const obj=JSON.parse(json);
+  console.log(obj);
+  const data=obj.data;
+  const type=obj.type;
+  console.log(data);
+  const options = {
+    bindto: "#chart",
+    axis: 
+      {x: {type: "category"}},
+    data: {
+      colors: {
+        Apples: "#ff0000",
+        Peaches: "#00ff00",
+        Bananas: "#0000ff",
+      },
+      type: type,
+      keys: {x: "name", value:["Jan", "Feb", "Mar"]},
+      json: data,
     },
-    type: "bar",
-    keys: {x: "name", value:["Jan", "Feb", "Mar"]},
-    json: [
-      
-        {
-          "name": "Apples",
-          "Jan": 327,
-          "Feb": 437,
-          "Mar": 111
-        },
-        {
-          "name": "Peaches",
-          "Jan": 555,
-          "Feb": 454,
-          "Mar": 222
-        },
-        {
-          "name": "Pears",
-          "Jan": 242,
-          "Feb": 864,
-          "Mar": 343
-        },
-        {
-          "name": "Bananas",
-          "Jan": 222,
-          "Feb": 454,
-          "Mar": 434
-        }
-      
-    ],
-  },
+  };
+  const chart = c3.generate(options);
 };
-const chart = c3.generate(options);
