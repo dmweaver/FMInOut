bd39733f8464a146c133bbddec057c70c25ee223
weaverd <david.weaver@dpird.wa.gov.au>
2022-10-15 07:21:45 +0800

    Added OnClick and Config from FM

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ window.loadChart=function(json){
   console.log(json);
   const obj=JSON.parse(json);
 
-  const obj1=JSON.parse(obj.colours);
+  // const obj1=JSON.parse(obj.colours);
   // const obj1 = json.replace(/\\/g, "");
-  console.log(obj1);
+  // console.log(obj1);
 
-
+const productsArray=["Apples", "Peaches", "Pears", "Bananas"];
 
   console.log(obj);
   const data=obj.data;
@@ -21,8 +21,17 @@ window.loadChart=function(json){
   console.log(height);
   const space=obj.space;
   console.log(space);
-  const colours=obj.colours;
-  console.log(colours);
+  const AppleColour=obj.AppleColour;
+  const PeachColour=obj.PeachColour;
+  const BananaColour=obj.BananaColour;
+  console.log (AppleColour);
+  console.log (PeachColour);
+  console.log (BananaColour);
+
+// const{data, type, width, height, space}=obj
+
+  // const colours=obj.colours;
+  // console.log(colours);
   
 
   const options = {
@@ -42,15 +51,24 @@ window.loadChart=function(json){
         width: {
           ratio: 0.8,
         }},
-    data: {labels:true,
+    data: {
+      onclick: function(d) {
+        console.log(d);
+        const{index, name}=d;
+        const product = productsArray[index];
+        console.log(product);
+        const toSendToFM={name, product};
+        console.log(toSendToFM);
+        FileMaker.PerformScript("Chart Click", JSON.stringify (toSendToFM));
+       },
       colors: 
-      // colours
       {
-        Apples: "#ff0000",
-        Peaches: "#00ff00",
-        Bananas: "#0000ff",
-      }
-      ,
+        Apple: AppleColour,
+        Peaches: PeachColour,
+        Banans: BananaColour,
+      },
+      labels:true,
+      
       type: type,
       keys: {x: "name", value:["Jan", "Feb", "Mar"]},
       json: data,
